879392632870d9b2bbf30e3b2e6cb51f75faa092
jithu2023 <jithumon.j@kalvium.community>
2023-12-12 17:30:51 +0530

    up

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:"/think_react_pt_3/",
+  plugins: [react()],
+})
